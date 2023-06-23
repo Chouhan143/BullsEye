dc7f9b69391e368a97892058427cb29bd9915325
kapil kachhawa <kapilkachhawa397@gmail.com>
2023-06-23 19:05:42 +0530

    add Buy and overnight,intraday

--- Src/BuySrceens/Intraday.js
+++ Src/BuySrceens/Intraday.js
@@ -1,14 +1,218 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity, TouchableHighlight } from 'react-native'
+import React, { useState } from 'react'
+import { COLORS, icons } from '../../constants'
 
 const Intraday = () => {
+  const [text, setText] = useState('');
+  const [textT, setTextT] = useState('');
+  const [isPressed, setIsPressed] = useState(false);
+  const [isPressed1, setIsPressed1] = useState(false);
+  const [isPressed2, setIsPressed2] = useState(false);
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  const handleButtonPress = (buttonId) => {
+    setSelectedButton(buttonId);
+  };
+
+  const handlePress = () => {
+    setIsPressed1(!isPressed1);
+  };
+  const handlePress2 = () => {
+    setIsPressed2(!isPressed2);
+  };
+
+
+  const handlePressIn = () => {
+    setIsPressed(true);
+  };
+
+  const handlePressOut = () => {
+    setIsPressed(false);
+  };
+
+
+
+  const handleTextChange = (inputText) => {
+    // Remove any non-numeric characters from the input
+    const numericValue = inputText.replace(/[^0-9]/g, '');
+    setText(numericValue);
+  };
+  const handleTextChange1 = (inputText) => {
+    // Remove any non-numeric and non-decimal characters from the input
+    const numericValue = inputText.replace(/[^0-9.]/g, '');
+
+    // Restrict to 6 digits before the decimal point
+    const maxLength = 10; // 6 digits + 1 decimal point
+    if (numericValue.length > maxLength) {
+      return;
+    }
+
+    // Restrict to 2 decimal places
+    const decimalParts = numericValue.split('.');
+    if (decimalParts[1] && decimalParts[1].length > 2) {
+      return;
+    }
+
+    setTextT(numericValue);
+  };
+  const renderButton = (buttonId, buttonColor, title) => (
+    <TouchableHighlight
+      key={buttonId}
+      style={[styles.button, { backgroundColor: selectedButton === buttonId ? 'green' : buttonColor }]}
+      underlayColor="yellow"
+      onPress={() => handleButtonPress(buttonId)}
+    >
+      <View>
+        <Text>{title}</Text>
+      </View>
+    </TouchableHighlight>
+  );
   return (
-    <View>
-      <Text>Intraday</Text>
+    <View style={{ flex: 1, backgroundColor: COLORS.mainBgColor }}>
+      <View style={{ flexDirection: "row", justifyContent: 'space-around', marginTop: 20, marginLeft: 10 }} >
+        <View style={{}}>
+          <Text>Quantity</Text>
+
+          <TouchableOpacity
+            onPressIn={handlePressIn}
+            onPressOut={handlePressOut}
+            style={[
+              { borderColor: isPressed ? COLORS.lightGray3 : COLORS.lightGreen },
+            ]}
+          >
+            <View style={{
+              width: 160, height: 60, borderColor: COLORS.lightGray3, borderWidth: 0.5, borderRadius: 10,
+              marginTop: 10,
+            }}>
+              <TextInput style={{ fontSize: 18, marginLeft: 5 }} value={text}
+                onChangeText={handleTextChange}
+                keyboardType="numeric"
+                maxLength={7}  >
+              </TextInput>
+              <View style={{ marginLeft: 105, marginTop: -35 }}>
+                <Text style={{ fontSize: 18 }}>Share</Text>
+              </View>
+
+            </View>
+          </TouchableOpacity>
+
+        </View>
+        <View style={{}}>
+          <Text style={{ marginLeft: 10 }}>Buying Price</Text>
+          <View style={{
+            width: 160, height: 60, borderColor: COLORS.lightGray3, borderWidth: 0.5, borderRadius: 10,
+            margin: 10,
+          }}>
+            <TextInput style={{ fontSize: 18, marginLeft: 5 }}
+              value={textT}
+              onChangeText={handleTextChange1}
+              keyboardType="numeric" // Set the keyboard type to numeric
+
+            />
+            <TouchableOpacity>
+              <View style={{ marginLeft: 130, marginTop: -35 }}>
+                <Image source={icons.licenses} style={{ width: 25, height: 25 }} />
+              </View>
+            </TouchableOpacity>
+
+          </View>
+        </View >
+      </View>
+      <View style={{ borderBottomColor: "#BBC7CF", borderBottomWidth: 0.5, marginTop: 20 }}></View>
+      <View style={{ marginLeft: 10, marginTop: 20 }}>
+        <Text style={{ fontSize: 20, fontWeight: '800', color: '#000' }}>Order type</Text>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+
+        <View style={styles.container}>
+          {renderButton(1, 'white', title = "Limit")}
+          <View style={{ marginLeft: 20 }}>
+            {renderButton(2, 'white', title = "Market")}
+          </View>
+          <View style={{ marginLeft: 20 }}>
+            {renderButton(3, 'white', title = "SL-LMT")}
+          </View>
+          <View style={{ marginLeft: 20 }}>
+            {renderButton(4, 'gray', title = "SL-MKT")}
+          </View>
+
+        </View>
+
+      </View>
+      <View style={{ marginLeft: 20, flexDirection: 'row', justifyContent: 'space-between' }}>
+        <View style={{ marginTop: 20, }}>
+          <Text style={{ fontSize: 15 }}>Stoploss trigger</Text>
+        </View>
+        <View style={{
+          width: 160, height: 60, borderColor: COLORS.lightGray3, borderWidth: 0.5, borderRadius: 10,
+          margin: 10,
+        }}>
+          <TextInput style={{ fontSize: 18, marginLeft: 5 }}
+            value={textT}
+            onChangeText={handleTextChange1}
+            keyboardType="numeric" // Set the keyboard type to numeric
+
+          />
+
+        </View>
+      </View>
+      <View style={{ borderBottomColor: "#BBC7CF", borderBottomWidth: 0.5, marginTop: 10 }}></View>
+
+      <View style={{ marginLeft: 10, marginTop: 20 }}>
+        <Text style={{ fontSize: 20, fontWeight: '800', color: '#000' }}>Product tpye</Text>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+
+        <View style={{ width: 70, height: 70, marginLeft: 20, marginTop: 20 }}>
+          {renderButton(5, 'white', title = "NRML")}
+
+        </View>
+
+      </View>
+      <View>
+        <View style={{ width: 80, height: 70, marginLeft: 20, marginTop: -10 }}>
+          {renderButton(6, 'green', title = "Add Cash")}
+
+
+        </View>
+
+      </View>
+      <View style={{ borderBottomColor: "#BBC7CF", borderBottomWidth: 0.5, marginTop: -10 }}></View>
+
+      <TouchableOpacity>
+        <View style={{
+          width: 300, height: 50, backgroundColor: 'green', marginTop: 20,
+          alignItems: 'center', justifyContent: 'center', alignSelf: 'center', borderRadius: 15
+        }}>
+          <Text style={{ fontSize: 20 }}>
+            Buy
+          </Text>
+
+        </View>
+      </TouchableOpacity>
+
+
+
+
     </View>
+
+
+
   )
+
 }
+export default Intraday;
 
-export default Intraday
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginLeft: 20, marginTop: 20,
+    flexDirection: 'row',
 
-const styles = StyleSheet.create({})
+  },
+  button: {
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+});
--- Src/ProfilePages/Userprofile.js
+++ Src/ProfilePages/Userprofile.js
@@ -7,112 +7,114 @@ const UserProfile = (onPress) => {
 
 
     return (
-        <View style={{ flex: 1, backgroundColor: COLORS.mainBgColor }}>
+        <ScrollView>
+            <View style={{ flex: 1, backgroundColor: COLORS.mainBgColor }}>
 
-            <View style={{ backgroundColor: COLORS.mainBgColor, marginTop: 10 }}>
-                <View style={{
-                    height: 130, backgroundColor: COLORS.bgColor, width: '95%', justifyContent: 'space-between',
-                    borderBottomWidth: 0.3, borderBottomColor: COLORS.lightGray3, alignSelf: 'center', flexDirection: 'row', borderRadius: 10
-                }}>
-                    <View style={{ marginTop: 25 }}>
-                        <Text style={{ fontSize: 25, marginLeft: 15, }}>kapil kachhawa</Text>
-                        <Text style={{ fontSize: 15, marginLeft: 15 }}>kas25014</Text>
+                <View style={{ backgroundColor: COLORS.mainBgColor, marginTop: 10 }}>
+                    <View style={{
+                        height: 130, backgroundColor: COLORS.bgColor, width: '95%', justifyContent: 'space-between',
+                        borderBottomWidth: 0.3, borderBottomColor: COLORS.lightGray3, alignSelf: 'center', flexDirection: 'row', borderRadius: 10
+                    }}>
+                        <View style={{ marginTop: 25 }}>
+                            <Text style={{ fontSize: 25, marginLeft: 15, }}>kapil kachhawa</Text>
+                            <Text style={{ fontSize: 15, marginLeft: 15 }}>kas25014</Text>
 
-                    </View>
+                        </View>
 
-                    <View
-                        style={{
-                            width: 50,
-                            height: 50,
-                            borderRadius: 50,
-                            marginTop: 20,
-                            marginRight: 50
-                        }}>
-                        <Image
-                            source={icons.profile}
-                            style={{
-                                width: 70,
-                                height: 70,
-                                borderWidth: 1,
-                                borderColor: '#000',
-                                borderRadius: 50,
-                            }}
-                        />
                         <View
                             style={{
-                                width: 30,
-                                height: 12,
-                                backgroundColor: 'red',
-                                position: 'absolute',
-                                right: -20,
-                                top: 40,
-                                borderRadius: 15,
+                                width: 50,
+                                height: 50,
+                                borderRadius: 50,
+                                marginTop: 20,
+                                marginRight: 50
                             }}>
-                            <TouchableOpacity>
-                                <Text style={{ color: '#fff', fontSize: 9, alignSelf: 'center' }}>
-                                    Edit
-                                </Text>
-                            </TouchableOpacity>
+                            <Image
+                                source={icons.profile}
+                                style={{
+                                    width: 70,
+                                    height: 70,
+                                    borderWidth: 1,
+                                    borderColor: '#000',
+                                    borderRadius: 50,
+                                }}
+                            />
+                            <View
+                                style={{
+                                    width: 30,
+                                    height: 12,
+                                    backgroundColor: 'red',
+                                    position: 'absolute',
+                                    right: -20,
+                                    top: 40,
+                                    borderRadius: 15,
+                                }}>
+                                <TouchableOpacity>
+                                    <Text style={{ color: '#fff', fontSize: 9, alignSelf: 'center' }}>
+                                        Edit
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
                         </View>
+
                     </View>
 
                 </View>
 
-            </View>
-
-            <View style={{
-                flexDirection: 'row', justifyContent: 'space-between',
-                paddingLeft: 20, paddingRight: 20, marginTop: 30
-            }}>
-                <Text style={{ fontSize: 17 }}>
-                    Password
-                </Text>
-                <TouchableOpacity>
-                    <Text style={{ fontSize: 17, color: 'blue' }}>
-                        change
+                <View style={{
+                    flexDirection: 'row', justifyContent: 'space-between',
+                    paddingLeft: 20, paddingRight: 20, marginTop: 30
+                }}>
+                    <Text style={{ fontSize: 17 }}>
+                        Password
                     </Text>
-                </TouchableOpacity>
-            </View>
-            <View style={{
-                flexDirection: 'row', justifyContent: 'space-between', marginTop: 40,
-                paddingLeft: 20, paddingRight: 20
-            }}>
-                <Text>E-mail</Text>
-                <Text>kk12345@gmail.com</Text>
-            </View>
-            <View style={{
-                flexDirection: 'row', justifyContent: 'space-between', marginTop: 40,
-                paddingLeft: 20, paddingRight: 20
-            }}>
-                <Text>Phone</Text>
-                <Text>88*59****5325.com</Text>
-            </View>
-            <View style={{
-                flexDirection: 'row', justifyContent: 'space-between', marginTop: 40,
-                paddingLeft: 20, paddingRight: 20
-            }}>
-                <Text>PAN</Text>
-                <Text>kwd**47de**</Text>
-            </View>
-            <View style={{ margin: 20, }}>
-                <View style={{marginTop:20}}>
-                    <Text style={{ fontSize: 15 }}>Bank Account</Text>
-
+                    <TouchableOpacity>
+                        <Text style={{ fontSize: 17, color: 'blue' }}>
+                            change
+                        </Text>
+                    </TouchableOpacity>
                 </View>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
-                    <Text>ICICI</Text>
-                    <Text>12454***45***58**</Text>
+                <View style={{
+                    flexDirection: 'row', justifyContent: 'space-between', marginTop: 40,
+                    paddingLeft: 20, paddingRight: 20
+                }}>
+                    <Text>E-mail</Text>
+                    <Text>kk12345@gmail.com</Text>
                 </View>
+                <View style={{
+                    flexDirection: 'row', justifyContent: 'space-between', marginTop: 40,
+                    paddingLeft: 20, paddingRight: 20
+                }}>
+                    <Text>Phone</Text>
+                    <Text>88*59****5325.com</Text>
+                </View>
+                <View style={{
+                    flexDirection: 'row', justifyContent: 'space-between', marginTop: 40,
+                    paddingLeft: 20, paddingRight: 20
+                }}>
+                    <Text>PAN</Text>
+                    <Text>kwd**47de**</Text>
+                </View>
+                <View style={{ margin: 20, }}>
+                    <View style={{ marginTop: 20 }}>
+                        <Text style={{ fontSize: 15 }}>Bank Account</Text>
+
+                    </View>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
+                        <Text>ICICI</Text>
+                        <Text>12454***45***58**</Text>
+                    </View>
+                </View>
+
+
+
+
+
+
+
+
             </View>
-
-
-
-
-
-
-
-
-        </View>
+        </ScrollView>
     )
 
 }
