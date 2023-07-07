2763a24e2d6ec05aba6e2f05fcb8ac35676cccde
kapil kachhawa <kapilkachhawa397@gmail.com>
2023-07-07 11:41:09 +0530

    ahf

--- Nav.js
+++ Nav.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import {NavigationContainer} from '@react-navigation/native';
+import React, { useState } from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 import SignIn from './screens/SignIn';
 import Funds from './Src/ProfilePages/Funds';
@@ -12,8 +12,8 @@ import SearchData from './Src/SearchData';
 import LoginScreen from './Src/LoginScreen';
 import RegisterScreen from './Src/RegisterScreen';
 import SellScreen from './Src/BuySrceens/SellScreen';
-import {Home} from './screens';
-import EmailVarification from './Src/BuySrceens/EmailVarification';
+import { Home } from './screens';
+import Button1 from './Src/BuySrceens/Button';
 import Email2 from './Src/BuySrceens/Email2';
 import Mobile from './Src/BuySrceens/Mobile';
 import Mobile2 from './Src/BuySrceens/Mobile2';
@@ -52,7 +52,8 @@ const Nav = () => {
         <Stack.Screen
           name="MainLayout"
           component={Tabs}
-          options={{headerShown: false}}
+          options={{ headerShown: false }}
+
         />
         <Stack.Screen
           options={{
@@ -62,39 +63,41 @@ const Nav = () => {
           component={Funds}
         />
         <Stack.Screen
-          options={{title: 'Profile', headerShown: false}}
+          options={{ title: 'Profile', headerShown: false }}
           name="UserProfile"
           component={UserProfile}
         />
-        <Stack.Screen name="SearchData" component={SearchData} />
         <Stack.Screen
-          options={{title: 'Setting', headerShown: false}}
+          name="SearchData"
+          component={SearchData}
+        />
+        <Stack.Screen
+          options={{ title: 'Setting', headerShown: false }}
           name="Setting"
           component={Setting}
         />
         <Stack.Screen
-          options={{title: 'BuyScreen', headerShown: false}}
+          options={{ title: 'BuyScreen', headerShown: false }}
           name="BuyScreen"
           component={BuySrceen}
         />
 
         <Stack.Screen
-          options={{title: 'SellScreen', headerShown: false}}
+          options={{ title: 'SellScreen', headerShown: false }}
           name="SellScreen"
           component={SellScreen}
         />
-     
+        {/* <Stack.Screen
+          options={{ title: 'Home', headerShown: false }}
+          name="Home"
+          component={Home}add
+        /> */}
 
-         <Stack.Screen
-<<<<<<< HEAD
-          options={{ title: 'button', headerShown: true }}
-          name="Button1"
-          component={Button1}
-=======
+        <Stack.Screen
           options={{ title: 'EmailVarification', headerShown: false }}
           name="email"
           component={EmailVarification}
->>>>>>> 3fb78a811219d9ab3acf132167196dad38e15cb2
+
         />
         <Stack.Screen
           options={{ title: 'Email2', headerShown: true }}
@@ -110,7 +113,7 @@ const Nav = () => {
           options={{ title: 'Mobile2', headerShown: false }}
           name="Mobile2"
           component={Mobile2}
-        /> 
+        />
         <Stack.Screen
           options={{ title: 'Document', headerShown: false }}
           name="Document"
